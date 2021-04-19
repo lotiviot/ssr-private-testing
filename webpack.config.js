ae260d46572f015702cb221f5b882cdef53f3ea0
Tristan Lotivio <tclotivio@outlook.com>
2021-04-18 21:48:10 -0400

    one last test

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
+const currentTask = process.env.npm_lifecycle_event
 var webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-module.exports = {
-    mode: 'none',
+const config = {
+    mode: 'development',
     entry: './src/index.js',
     module: {
         rules: [
@@ -9,14 +11,23 @@ module.exports = {
             {test: /\.jsx$/, loader: 'babel-loader', exclude:/node_modules/},
             {test: /\.(scss|css)$/ , use:['style-loader','css-loader', 'sass-loader']}
         ]
-    }, 
+    },
     plugins: [
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify('development')
-        })
+        // new webpack.DefinePlugin({
+        //     'process.env.NODE_ENV': JSON.stringify('development')
+        // })
     ],
     output:{
         filename: 'bundle.js',
         path: __dirname + '/public'
     }
 }
+
+if(currentTask == "build"){
+    config.mode = "production"
+    config.module.rules[2].use[0] = MiniCssExtractPlugin.loader
+    config.plugins.push(new MiniCssExtractPlugin({filename: "main.css"}))
+
+}
+
+module.exports = config
